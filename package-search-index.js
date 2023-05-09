ae7f9db86df64f9abee713f5a578e6b9534ff277
justinwb <justinwb@users.noreply.github.com>
2023-05-09 23:49:12 +0000

    deploy: c52ce89e23394b1c3ba47665a9ab4e7e9c1cbdc4

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.janeirodigital.sai.rdfutils"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.janeirodigital.sai.rdfutils"}];updateSearchResults();
--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.janeirodigital.sai.rdfutils","l":"RdfUtils"},{"p":"com.janeirodigital.sai.rdfutils","l":"SaiRdfException"},{"p":"com.janeirodigital.sai.rdfutils","l":"SaiRdfNotFoundException"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.janeirodigital.sai.rdfutils","l":"RdfUtils"},{"p":"com.janeirodigital.sai.rdfutils","l":"SaiRdfException"},{"p":"com.janeirodigital.sai.rdfutils","l":"SaiRdfNotFoundException"}];updateSearchResults();
